eff3d803a78c0ede239a0bf6b9af0f2dbfd20e6c
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-10-14 12:55:33 +0330

    fix src

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ iconRes.addEventListener('click', iconChange);
 function iconChange(event) {
     event.preventDefault();
     if (iconRes.src === 'https://amirhghorbani.github.io/darmanito-pj-1/img/drop-down-icon.png') {
-        iconRes.src = './img/X-icon.png';
+        iconRes.src = 'https://amirhghorbani.github.io/darmanito-pj-1/img/X-icon.png';
         dropdownNavBar.style.display = 'block';
         console.log(iconRes.src)
     }else {
-        iconRes.src = 'https://amirhghorbani.github.io/darmanito-pj-1/img/X-icon.png';
+        iconRes.src = 'https://amirhghorbani.github.io/darmanito-pj-1/img/drop-down-icon.png';
         dropdownNavBar.style.display = 'none';
     }
      
