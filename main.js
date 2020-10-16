f2c77cd4661182383a9ba54ecd4d3f8007afbc71
amirhossein ghorbani <amirhghorbani@amirhosseins-MacBook-Pro.local>
2020-10-16 20:01:35 +0330

    finish responsive for all devices

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const iconRes = document.getElementById('drop-icon');
 const dropdownNavBar = document.querySelector('#dropdown-nav-bar');
-console.log(dropdownNavBar);
 console.log(iconRes.src)
 
 iconRes.addEventListener('click', iconChange);
@@ -10,7 +9,7 @@ function iconChange(event) {
     if (iconRes.src === 'https://amirhghorbani.github.io/darmanito-pj-1/img/drop-down-icon.png') {
         iconRes.src = 'https://amirhghorbani.github.io/darmanito-pj-1/img/X-icon.png';
         dropdownNavBar.style.display = 'block';
-        console.log(iconRes.src)
+        
     }else {
         iconRes.src = 'https://amirhghorbani.github.io/darmanito-pj-1/img/drop-down-icon.png';
         dropdownNavBar.style.display = 'none';
